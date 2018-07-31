541406aea24730438d6e21287a80057e43f60de9
Gabriel Haggstrom <gshaggstrom@gmail.com>
2018-07-31 16:16:33 -0300

    Implement page to create a counter.

---
+++ create.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let titleElement = document.querySelector("#title");
+let dateElement = document.querySelector("#date");
+
+let outputElement = document.querySelector("#output");
+let linkElement = document.querySelector("#link");
+
+[titleElement, dateElement].forEach((inputElement) => {
+  inputElement.addEventListener("input", updateURL);
+});
+
+updateURL();
+
+
+function updateURL() {
+  let title = titleElement.value;
+  let date = dateElement.value;
+
+  if (date === "") {
+    outputElement.style.display = "none";
+  } else {
+    let link = `http://localhost:8080/?title=${title}&date=${date}`;
+
+    linkElement.href = link;
+    linkElement.innerHTML = link;
+
+    outputElement.style.display = "block";
+  }
+}
